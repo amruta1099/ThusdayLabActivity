a916fbc91d56e48a5365d14df608164ea56b5d46
amruta1099 <118454501+amruta1099@users.noreply.github.com>
2022-11-22 18:40:56 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+package com.question3;
+public class Pattern {
+	
+	public static void main(String a[])
+	{
+		int i;
+		int j;
+		int n=1;
+		
+		for(i=1; i<=5; i++)
+		{
+			for(j=1; j<=i; j++)
+			{
+				System.out.print(n);
+				n++;
+			}
+			System.out.print("\n");
+		}
+	}
+}
